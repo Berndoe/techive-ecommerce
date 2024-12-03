5aff7f189bc8f6b0deb61b7781379880b8377586
Berndoe <88336484+Berndoe@users.noreply.github.com>
2024-12-03 17:16:58 +0000

    Update db_cred.php

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,10 +1,10 @@
 <?php
 //Database credentials
-define("SERVER", "localhost");
-define("USERNAME", "hotel_user");
-define("PASSWD", "admin");
-define("DATABASE", "techive");
+define("SERVER", "");
+define("USERNAME", "");
+define("PASSWD", "");
+define("DATABASE", "");
 
-// might not work. if it doesn't change username to hotel_user and pass to admin
+
 
 ?>
